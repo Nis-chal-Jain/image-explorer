498336a133663779198c4b0b8abd8b1232a20c28
Nischal Jain <89198650+Nis-chal-Jain@users.noreply.github.com>
2024-08-25 17:37:24 +0000

    minor edit

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -8,11 +8,11 @@ function Favorites() {
       <NavMenu />
       <div className=" text-5xl">Favorites</div>
       <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-4 space-y-4 p-4 m-auto">
-        {fav.map(function (data) {
+        {fav==null?<div>No Favorites to show..</div>:(fav.map(function (data) {
           return (
             <img src={data} alt="" className="rounded-md mx-auto" />
           )
-        })}
+        }))}
       </div>
 
     </>
